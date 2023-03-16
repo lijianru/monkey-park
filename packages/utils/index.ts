9035d2b96f85d3dc8cee685f87fcd44d80a7bec9
lijainru <2219269374@qq.com>
2023-03-16 17:09:15 +0800

    add prettier config

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,7 +1,7 @@
 export function Accumulation(...value: number[]) {
-  return value.reduce((t, v) => t + v, 0)
+  return value.reduce((t, v) => t + v, 0);
 }
 
 export function Multiplication(...value: number[]) {
-  return value.reduce((t, v) => t * v, 1)
+  return value.reduce((t, v) => t * v, 1);
 }
